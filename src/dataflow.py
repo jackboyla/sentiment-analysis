b258516f23e220f422df2f3658c465f018afef6c
Jack Boylan <boylan.jack@gmail.com>
2023-05-08 19:57:43 +0100

    Replaces data pipeline with Lightning DataModule

--- src/dataflow.py
+++ src/dataflow.py
@@ -1,9 +1,17 @@
 
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 import torch.nn.functional as F
 from torch.nn.utils.rnn import pad_sequence
 import re
+import lightning as L
+from sklearn.model_selection import train_test_split
+
+import pandas as pd
+from transformers import CanineTokenizer
+
+import multiprocessing
+
 
 def remove_urls(text):
     url_pattern = re.compile(r'https?://\S+|www\.\S+')
@@ -52,3 +60,92 @@ def collate_fn(batch, input_pad_token_id=0):
         
 
     return {'input_ids': input_ids, 'attention_mask': attention_masks}, torch.tensor(labels)
+
+
+class TweetDataModule(L.pytorch.LightningDataModule):
+    def __init__(self, cfg):
+        super().__init__()
+        self.cfg = cfg
+        try:
+            self.num_workers = multiprocessing.cpu_count()
+        except:
+            self.num_workers = 0
+
+        # single sequence: [CLS] X [SEP]
+        self.tokenizer = CanineTokenizer.from_pretrained("google/canine-c", 
+                                                    return_tensors='pt')
+
+    def prepare_data(self):
+        """
+        https://pytorch-lightning.readthedocs.io/en/latest/data/datamodule.html#prepare-data
+        """
+
+        DATASET_COLUMNS = ["target", "ids", "date", "flag", "user", "text"]
+        df = pd.read_csv(
+            self.cfg.datafiles.data_dir,
+            encoding=self.cfg.datafiles.dataset_encoding,
+            names=DATASET_COLUMNS
+            )
+        decode_map = {0: 0, 4: 1}
+        def binarize_sentiment(label):
+            return decode_map[int(label)]
+
+        df['target'] = df.target.apply(lambda x: binarize_sentiment(x))
+
+        train_size = int(self.cfg.data_processing.train_size if self.cfg.data_processing.train_size > 1.0 else self.cfg.data_processing.train_size*len(df))
+        val_size = int(self.cfg.data_processing.val_size if self.cfg.data_processing.val_size > 1.0 else self.cfg.data_processing.val_size*len(df))
+        test_size = int(self.cfg.data_processing.test_size if self.cfg.data_processing.test_size > 1.0 else self.cfg.data_processing.test_size*len(df))
+
+        self.train_df, self.test_df = train_test_split(df, train_size=train_size+val_size, test_size=test_size, random_state=42, stratify=df['target'])
+        self.train_df, self.val_df = train_test_split(self.train_df, train_size=train_size, test_size=val_size, random_state=42, stratify=self.train_df['target'])
+
+        self.train_df.reset_index(inplace=True, drop=True)
+        self.val_df.reset_index(inplace=True, drop=True)
+        self.test_df.reset_index(inplace=True, drop=True)
+
+
+    def setup(self, stage: str):
+        """
+        Method to setup your datasets, here you can use whatever dataset class you have defined in Pytorch and prepare the data in order to pass it to the loaders later
+        https://pytorch-lightning.readthedocs.io/en/latest/data/datamodule.html#setup
+        """
+        if stage == "fit":
+            self.train_dataset = TweetDataset(self.train_df['text'], self.train_df['target'], self.tokenizer)
+            self.val_dataset = TweetDataset(self.val_df['text'], self.val_df['target'], self.tokenizer)
+            
+        # Assign test dataset for use in dataloader(s)
+        if stage == "test":
+            self.test_dataset = TweetDataset(self.test_df['text'], self.test_df['target'], self.tokenizer)
+
+        # if stage == "predict":
+        #     self.test_dataset = TweetDataset(self.test_df['text'], self.test_df['target'], self.tokenizer)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_dataset, 
+                              batch_size=self.cfg.hyperparameters.batch_size, 
+                              shuffle=True, 
+                              collate_fn=lambda b: collate_fn(b, input_pad_token_id=self.tokenizer.pad_token_id),
+                              num_workers=self.num_workers
+                              )
+
+    def val_dataloader(self):
+        return DataLoader(self.val_dataset, batch_size=self.cfg.hyperparameters.batch_size, shuffle=False, 
+                          collate_fn=lambda b: collate_fn(b, input_pad_token_id=self.tokenizer.pad_token_id),
+                          num_workers=self.num_workers
+                          )
+        
+
+    def test_dataloader(self):
+        return DataLoader(self.test_dataset, batch_size=self.cfg.hyperparameters.batch_size, shuffle=False, 
+                                     collate_fn=lambda b: collate_fn(b, input_pad_token_id=self.tokenizer.pad_token_id),
+                                     num_workers=self.num_workers
+                                     )
+
+
+    # def predict_dataloader(self):
+    #     return DataLoader(self.test_dataset, batch_size=self.cfg.hyperparameters.batch_size, shuffle=False, 
+    #                                  collate_fn=lambda b: collate_fn(b, input_pad_token_id=self.tokenizer.pad_token_id),
+    #                                  num_workers=self.num_workers
+    #                                  )
+
+    
--- src/train.py
+++ src/train.py
@@ -3,6 +3,7 @@
 import importlib
 import sys
 import os
+import torch
 from torch.utils.data import DataLoader
 import dataflow as dataflow
 import model as models
@@ -21,55 +22,78 @@ from lightning.pytorch.loggers import WandbLogger, CSVLogger
 import time
 import wandb
 
+if torch.cuda.is_available():
+    # Specify the device you want to check
+    device = torch.device("cuda:0")  # Change 0 to the index of your CUDA device if you have multiple GPUs
+
+    # Get the device properties
+    device_props = torch.cuda.get_device_properties(device)
+
+    ''' 
+        Check if the device has Tensor Cores
+        According to NVIDIA's documentation, 
+        Tensor Cores were introduced in the Volta architecture (CUDA Compute Capability 7.0 and above), 
+        so we check if the device's major version is 7 or greater and the minor version is 0 or greater.
+    '''
+    if device_props.major >= 7 and device_props.minor >= 0:
+        print("This device has Tensor Cores!\n Setting precision to medium...")
+        torch.set_float32_matmul_precision('medium' | 'high')
+    else:
+        print("This device does not have Tensor Cores :(")
+
+
 cfg_path = sys.argv[1]  # 'configs/local-canine-backbone.yaml'
 cfg = OmegaConf.load(cfg_path)
 
 server_log_file = sys.argv[2]
 
-DATASET_COLUMNS = ["target", "ids", "date", "flag", "user", "text"]
-df = pd.read_csv(
-    cfg.datafiles.data_dir,
-    encoding=cfg.datafiles.dataset_encoding,
-    names=DATASET_COLUMNS
-    )
 
-decode_map = {0: 0, 4: 1}
-def binarize_sentiment(label):
-    return decode_map[int(label)]
+dm = dataflow.TweetDataModule(cfg)
 
-df['target'] = df.target.apply(lambda x: binarize_sentiment(x))
+# DATASET_COLUMNS = ["target", "ids", "date", "flag", "user", "text"]
+# df = pd.read_csv(
+#     cfg.datafiles.data_dir,
+#     encoding=cfg.datafiles.dataset_encoding,
+#     names=DATASET_COLUMNS
+#     )
 
-train_size = int(cfg.data_processing.train_size if cfg.data_processing.train_size > 1.0 else cfg.data_processing.train_size*len(df))
-val_size = int(cfg.data_processing.val_size if cfg.data_processing.val_size > 1.0 else cfg.data_processing.val_size*len(df))
-test_size = int(cfg.data_processing.test_size if cfg.data_processing.test_size > 1.0 else cfg.data_processing.test_size*len(df))
+# decode_map = {0: 0, 4: 1}
+# def binarize_sentiment(label):
+#     return decode_map[int(label)]
 
-train_df, test_df = train_test_split(df, train_size=train_size+val_size, test_size=test_size, random_state=42, stratify=df['target'])
-train_df, val_df = train_test_split(train_df, train_size=train_size, test_size=val_size, random_state=42, stratify=train_df['target'])
+# df['target'] = df.target.apply(lambda x: binarize_sentiment(x))
 
-train_df.reset_index(inplace=True, drop=True)
-val_df.reset_index(inplace=True, drop=True)
-test_df.reset_index(inplace=True, drop=True)
+# train_size = int(cfg.data_processing.train_size if cfg.data_processing.train_size > 1.0 else cfg.data_processing.train_size*len(df))
+# val_size = int(cfg.data_processing.val_size if cfg.data_processing.val_size > 1.0 else cfg.data_processing.val_size*len(df))
+# test_size = int(cfg.data_processing.test_size if cfg.data_processing.test_size > 1.0 else cfg.data_processing.test_size*len(df))
 
+# train_df, test_df = train_test_split(df, train_size=train_size+val_size, test_size=test_size, random_state=42, stratify=df['target'])
+# train_df, val_df = train_test_split(train_df, train_size=train_size, test_size=val_size, random_state=42, stratify=train_df['target'])
 
-
-# single sequence: [CLS] X [SEP]
-tokenizer = CanineTokenizer.from_pretrained("google/canine-c", 
-                                            return_tensors='pt')
-
-# Build Datasets
-train_dataset = dataflow.TweetDataset(train_df['text'], train_df['target'], tokenizer)
-val_dataset = dataflow.TweetDataset(val_df['text'], val_df['target'], tokenizer)
-test_dataset = dataflow.TweetDataset(test_df['text'], test_df['target'], tokenizer)
+# train_df.reset_index(inplace=True, drop=True)
+# val_df.reset_index(inplace=True, drop=True)
+# test_df.reset_index(inplace=True, drop=True)
 
 
-# Build DataLoaders
-train_dataloader = DataLoader(train_dataset, 
-                              batch_size=cfg.hyperparameters.batch_size, 
-                              shuffle=True, 
-                              collate_fn=lambda b: dataflow.collate_fn(b, input_pad_token_id=tokenizer.pad_token_id)
-                              )
-val_dataloader = DataLoader(val_dataset, batch_size=cfg.hyperparameters.batch_size, shuffle=False, collate_fn=lambda b: dataflow.collate_fn(b, input_pad_token_id=tokenizer.pad_token_id))
-test_dataloader = DataLoader(test_dataset, batch_size=cfg.hyperparameters.batch_size, shuffle=False, collate_fn=lambda b: dataflow.collate_fn(b, input_pad_token_id=tokenizer.pad_token_id))
+
+# # single sequence: [CLS] X [SEP]
+# tokenizer = CanineTokenizer.from_pretrained("google/canine-c", 
+#                                             return_tensors='pt')
+
+# # Build Datasets
+# train_dataset = dataflow.TweetDataset(train_df['text'], train_df['target'], tokenizer)
+# val_dataset = dataflow.TweetDataset(val_df['text'], val_df['target'], tokenizer)
+# test_dataset = dataflow.TweetDataset(test_df['text'], test_df['target'], tokenizer)
+
+
+# # Build DataLoaders
+# train_dataloader = DataLoader(train_dataset, 
+#                               batch_size=cfg.hyperparameters.batch_size, 
+#                               shuffle=True, 
+#                               collate_fn=lambda b: dataflow.collate_fn(b, input_pad_token_id=tokenizer.pad_token_id)
+#                               )
+# val_dataloader = DataLoader(val_dataset, batch_size=cfg.hyperparameters.batch_size, shuffle=False, collate_fn=lambda b: dataflow.collate_fn(b, input_pad_token_id=tokenizer.pad_token_id))
+# test_dataloader = DataLoader(test_dataset, batch_size=cfg.hyperparameters.batch_size, shuffle=False, collate_fn=lambda b: dataflow.collate_fn(b, input_pad_token_id=tokenizer.pad_token_id))
 
 
 # if cfg.logging.wandb:
@@ -93,7 +117,8 @@ for logger, values in cfg.loggers.items():
 # csv_logger = CSVLogger(cfg.logging.log_dir, name=log_name, version=version)
 
 
-print(f"Training on {len(train_df)} examples...")
+# print(f"Training on {len(dm.train_df)} examples...")
+
 
 # early_stop = EarlyStopping(cfg.callbacks.early_stopping.monitor, 
 #                            patience=cfg.callbacks.early_stopping.patience, 
@@ -126,14 +151,14 @@ trainer = L.Trainer(logger=list(loggers.values()),
                     **cfg.hyperparameters.trainer
                     )
 
-classifier = models.SentimentClassifier(tokenizer=tokenizer, 
+classifier = models.SentimentClassifier(tokenizer=dm.tokenizer, 
                                         hyperparams=cfg.hyperparameters)
 
 # log gradients and model topology
 if 'wandb_logger' in loggers:
     loggers['wandb_logger'].watch(classifier)
 
-trainer.fit(classifier, train_dataloader, val_dataloader)
+trainer.fit(classifier, datamodule=dm)
 
 if 'wandb_logger' in loggers:
     loggers['wandb_logger'].experiment.unwatch(classifier)
@@ -145,7 +170,7 @@ with open(os.path.join(*[cfg.loggers.csv_logger.kwargs.save_dir,
                          'config.yaml']), 'w') as f:
     OmegaConf.save(config=cfg, f=f)
 
-trainer.test(classifier, dataloaders=test_dataloader)
+trainer.test(classifier, datamodule=dm)
 
 if 'wandb_logger' in loggers:
     # [optional] finish the wandb run, necessary in notebooks
