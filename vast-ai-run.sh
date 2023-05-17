bb744510732b6bdd129353ea5d16429f7ca18fd4
Jack Boylan <boylan.jack@gmail.com>
2023-05-17 17:31:40 +0100

    Updates transfer error in vast ai run script

--- vast-ai-run.sh
+++ vast-ai-run.sh
@@ -7,26 +7,23 @@ echo 'starting up'
 env | grep SLACK_HOOK >> /etc/environment;
 SLACK_HOOK=$(env | grep SLACK_HOOK | cut -d'=' -f2)
 
-
 # Setting up workspace
 cd /workspace
 git clone https://github.com/jackboyla/sentiment-analysis.git
 GIT_REPO_PATH=/workspace/sentiment-analysis
 
 # Define the list of config files to train models for
-configs=("cloud-canine-backbone.yaml") 
+configs=("canine-backbone.yaml") 
 
-pip install torch kaggle lightning tabulate transformers wandb omegaconf scikit-learn pandas
+pip install torch kaggle lightning tabulate transformers wandb omegaconf scikit-learn pandas gdown
+
 
 # Download data
 cd $GIT_REPO_PATH
-mkdir data
-cd data
-URL='https://storage.googleapis.com/kaggle-data-sets/2477/4140/bundle/archive.zip?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=gcp-kaggle-com%40kaggle-161607.iam.gserviceaccount.com%2F20230504%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20230504T191741Z&X-Goog-Expires=259200&X-Goog-SignedHeaders=host&X-Goog-Signature=49150b528cec87a1e719d2a8e289fb38bb9883ddf8a088014aef9a6cdc901f4e0ce0cf5ab5b0b6296e02ff7581280095d8da86b5a6be8771dde1cc1a4b3f8b10b7bae7da2bb112a19753c351425e061b6081596e617667237da44ee010f42994a76d8f6bc1a6ac589af8c5a58e3d8091e52ed1014a9564fbe05f4c3cac5ef114111d38b70baed4f494c84e5526e9140a6cb8548f40e3da59f345ec5f04c524f044b361ef780818f3c10032cf2a5a37d120d67a8e676e9f794fbb7adead0e436e3a42263620ffcd121f2aee869642e626da8059579552437d714371ad245cea72d222b5c1059581b8d6c8e14cf6cf4bc1b719e66db63ad46c9adecf6455fb2e52'
-wget ${URL} --no-check-certificate -O sentiment140.zip
+gdown --fuzzy 'https://drive.google.com/drive/folders/1qZ1yguFcr6VGA-rrgrxtdjkb0WLXBcQO?usp=sharing' -O ./data --folder
 sudo apt-get install -y zip
 sudo apt-get install -y unzip
-unzip sentiment140.zip
+
 
 # https://github.com/dutchcoders/transfer.sh/blob/main/examples.md#using-curl
 transfer() {
@@ -77,8 +74,8 @@ function zip_and_transfer_artifacts() {
         # Get the base name of the path
         base_name=$(basename "$path")
 
-        # Copy the file or directory to the temporary directory
-        cp -R "$path" "${TEMP_DIR}/${base_name}"
+        # Move the file or directory to the temporary directory
+        mv -R "$path" "${TEMP_DIR}/${base_name}"
     done
 
     # Zip the temporary directory and its contents
@@ -88,16 +85,28 @@ function zip_and_transfer_artifacts() {
     local ZIP_NAME=${DATE_TIME}_${CONFIG}.zip
     zip -r "$ZIP_NAME" .
 
-    # Transfer the zip file using the transfer function
-    local TRANSFER_LINK
-    TRANSFER_LINK=$(transfer "${TEMP_DIR}/${ZIP_NAME}")
-
     # Capture the output of ls -1 "$TEMP_DIR" in a variable
     local TEMP_DIR_LS
     TEMP_DIR_LS=$(ls -1 "$TEMP_DIR")
 
     # Echo a list of all the files and directories that have been zipped
     echo -e "The following files and directories have been zipped: \n${TEMP_DIR_LS}"
+
+    # Record the start time
+    transfer_start_time=$(date +%s)
+
+    # Transfer the zip file using the transfer function
+    local TRANSFER_LINK
+    TRANSFER_LINK=$(transfer "${TEMP_DIR}/${ZIP_NAME}")
+
+    # Record the end time
+    transfer_end_time=$(date +%s)
+
+    # Calculate the elapsed time
+    transfer_elapsed_time=$(($transfer_end_time - $transfer_start_time))
+
+    # Echo the elapsed time
+    echo "Transfer Elapsed time: $transfer_elapsed_time seconds"
 
     # Remove the temporary directory and zip file
     rm -rf "$TEMP_DIR"
@@ -109,7 +118,6 @@ function zip_and_transfer_artifacts() {
 }
 
 
-
 cd $GIT_REPO_PATH
 
 # Loop through the config files
@@ -119,18 +127,14 @@ do
     DATE_TIME=$(date -u +'%Y-%m-%d__%H-%M-UTC')
     
     # Run the train.py script with the current config file and send output to logs
+    echo "Training Starting..."
     BASH_LOG="server_log_${DATE_TIME}_${config%.yaml}.log"
-    python src/train.py "configs/${config}" "${BASH_LOG}" &> "${BASH_LOG}"
+    python src/train.py --cfg_path "configs/${config}" --server_log_file "${BASH_LOG}" &> "${BASH_LOG}"
 
     # Define the ARTIFACTS you want transferred
-    ARTIFACTS=(${GIT_REPO_PATH}/configs/${config})
 
-    ARTIFACTS+=(${GIT_REPO_PATH}/${BASH_LOG})
-
-    # Save CSV logs
-    # Get the most recently created directory in logs/lightning_logs/
-    latest_dir=$(ls -1t ${GIT_REPO_PATH}/logs/lightning_logs/ | head -n 1)
-    ARTIFACTS+=("${GIT_REPO_PATH}/logs/lightning_logs/${latest_dir}")
+    # Save the training logs
+    ARTIFACTS=(${GIT_REPO_PATH}/${BASH_LOG})
 
     # Save model CHECKPOINTS
     latest_dir=$(ls -1t ${GIT_REPO_PATH}/wandb-sentiment-analysis/ | head -n 1)
@@ -140,9 +144,13 @@ do
     latest_dir=$(ls -1t ${GIT_REPO_PATH}/wandb/ | head -n 1)
     ARTIFACTS+=("${GIT_REPO_PATH}/wandb/${latest_dir}")
     
-
+    # Zip, Transfer and Remove Relevant files/dirs
     zip_and_transfer_artifacts "${config%.yaml}" "${SLACK_HOOK}" "${ARTIFACTS[@]}"
 done
+
+
+# Wait for all background processes to finish before destroying the instance
+wait
 
 
 # Record the end time
@@ -158,10 +166,11 @@ post_to_slack "Script finishing! Elapsed Time: ${elapsed_time}" "INFO" "${SLACK_
 
 
 # Destroy Instance (from https://vast.ai/faq#Instances)
+cd /
+
 cat ~/.ssh/authorized_keys | md5sum | awk '{print $1}' > /ssh_key_hv; echo -n $VAST_CONTAINERLABEL | md5sum | awk '{print $1}' > /instance_id_hv; head -c -1 -q /ssh_key_hv /instance_id_hv > ~/.vast_api_key;
 
-mkdir ${GIT_REPO_PATH}/vast
-sudo apt-get install -y wget; wget https://raw.githubusercontent.com/vast-ai/vast-python/master/vast.py -O ./vast; chmod +x ./vast;
+sudo apt-get install -y wget; wget https://raw.githubusercontent.com/vast-ai/vast-python/master/vast.py -O vast; chmod +x vast;
 
 ./vast destroy instance ${VAST_CONTAINERLABEL:2}
 
