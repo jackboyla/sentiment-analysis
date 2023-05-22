337dc582332f422799f39bd9aab2fee6236a924d
Jack Boylan <boylan.jack@gmail.com>
2023-05-22 13:24:50 +0100

    Finishes predict.py

--- sentiment_classification/predict.py
+++ sentiment_classification/predict.py
@@ -16,7 +16,7 @@ def main():
     logger = utils.create_logger(__name__)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--cfg-path", type=str, help="Config path for training")
+    parser.add_argument("--cfg-path", type=str, help="Config path of model you want to use for inference")
     parser.add_argument("--input", type=str, help="File to make predictions on")
     parser.add_argument("--output", type=str, help="File to write predictions to")
     args = parser.parse_args()
@@ -26,38 +26,41 @@ def main():
 
     logger.info(f"Loaded config from {cfg_path}")
 
-    # Load Tokenizer
-    with open(cfg.datafiles.tokenizer_file, 'rb') as f:
-        tokenizer = pickle.load(f)
-
     # Get latest checkpoint
     model_ckpts = glob.glob(f'{cfg.datafiles.saved_ckpt_dir}/*') 
     latest_ckpt = max(model_ckpts, key=os.path.getctime)
     logger.info(f"Found latest checkpoint at {latest_ckpt}")
     
-    # Load model from checkpoint
-    model = models.SentimentClassifier.load_from_checkpoint(latest_ckpt, 
-                                                            tokenizer=tokenizer, 
-                                                            hyperparams=cfg.hyperparameters)
+    # Load model from architecture and state_dict
+    with open(cfg.datafiles.architecture_save_path, 'rb') as f:
+        model = pickle.load(f)
+
+    latest_ckpt = torch.load(latest_ckpt)
+    model.load_state_dict(latest_ckpt['state_dict'])
     model.eval()
 
+    # Begin Inference
     outputs = []
-    with open(args.input, 'r') as f:
+    label_decode_map = {v: k for k, v in cfg.data_processing.label_decode_map.items()}
+    with open(args.input, 'r', encoding="utf8") as f:
+
         with torch.no_grad():
             # Iterate through each line in the input file
             for line in f:
 
                 # Pass the input through the model
-                out = model(line)
+                out = model(line)[0]
+                # print(f"Text: {line}Prediction: {out}\n")
         
-                # Convert the outputs to a readable format if needed
-                out = cfg.data_processing.decode_label_map[out]
+                # Convert the outputs to a readable format
+                out = label_decode_map[out]
                 outputs.append(out)
         
 
+    # Write Predictions to output file
     with open(args.output, 'w') as f:
-        # Write the output to the output file
-        f.write([out + '\n' for out in outputs])
+        for prediction in outputs:
+            f.write(f"{prediction}\n")
 
     logger.info(f"Saved predictions to {args.output}")
 
