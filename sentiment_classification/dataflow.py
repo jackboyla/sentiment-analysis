d6206f6ed7bbb7a238080e80a5302a2bfb61762f
Jack Boylan <boylan.jack@gmail.com>
2023-05-23 11:54:09 +0100

    Adds random dataset and src_key_padding_mask input

--- sentiment_classification/dataflow.py
+++ sentiment_classification/dataflow.py
@@ -1,7 +1,7 @@
 
 import torch
-from torch.utils.data import Dataset, DataLoader
-import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader, WeightedRandomSampler
+import numpy as np
 from torch.nn.utils.rnn import pad_sequence
 import re
 import utils
@@ -27,12 +27,16 @@ def remove_mentions_hashtags(text):
     text = hashtag_pattern.sub(r'', text)
     return text
 
+def make_lowercase(text):
+    return text.lower()
+
 class TweetDataset(Dataset):
     def __init__(self, tweets, labels, tokenizer):
         self.tweets = tweets
         self.tweets = self.tweets.apply(convert_html_entities)
         self.tweets = self.tweets.apply(remove_urls)
         self.tweets = self.tweets.apply(remove_mentions_hashtags)
+        # self.tweets = self.tweets.apply(make_lowercase)
         self.labels = labels
         self.tokenizer = tokenizer
 
@@ -47,7 +51,34 @@ class TweetDataset(Dataset):
         encoded_input.update({'tweet': tweets, 'labels': labels})
         return encoded_input
     
-def collate_fn(batch, input_pad_token_id):
+
+class RandomDataset(Dataset):
+    def __init__(self, size, num_samples, num_classes=2):
+        self.len = num_samples
+        assert num_samples % num_classes == 0, "num_samples % num_classes should = 0"
+        sample_subset = num_samples // num_classes
+
+        # self.data = np.random.randn(num_samples, size)
+        self.data  = np.concatenate((np.full((num_samples, size), fill_value=6), 
+                                     np.full((num_samples, size), fill_value=7),
+                                     np.full((num_samples, size), fill_value=9)),
+                                     axis=0)
+        
+
+        self.labels = np.concatenate((np.zeros(sample_subset), np.ones(sample_subset), np.full((num_samples), fill_value=2)))
+
+    def __getitem__(self, index):
+        input_dict = {'input_ids': self.data[index],
+                      'attention_mask' : self.data[index],
+                      'labels': self.labels[index]
+                      }
+        return input_dict
+
+    def __len__(self):
+        return self.len
+
+    
+def collate_fn(batch, input_pad_token_id=0.0):
     '''
     dynamically padding input sequences per batch
     batch is alist of dicts
@@ -57,11 +88,30 @@ def collate_fn(batch, input_pad_token_id):
     input_ids = [torch.tensor(sample['input_ids']) for sample in batch]
     labels = [sample['labels'] for sample in batch]
 
-    input_ids = pad_sequence(input_ids, batch_first=True, padding_value=input_pad_token_id)
-    attention_masks = pad_sequence(attention_masks, batch_first=True, padding_value=0.0)
-        
+    # Calculate the lengths of input sequences
+    input_lengths = torch.tensor([len(seq) for seq in input_ids])
 
-    return {'input_ids': input_ids, 'attention_mask': attention_masks}, torch.tensor(labels)
+    input_ids = pad_sequence(input_ids, batch_first=True, padding_value=input_pad_token_id)
+    attention_masks = pad_sequence(attention_masks, batch_first=True, padding_value=input_pad_token_id)
+
+    # Sort the sequences, masks, and labels based on lengths in descending order
+    sorted_lengths, sorted_indices = torch.sort(input_lengths, descending=True)
+    input_ids = input_ids[sorted_indices]
+    attention_masks = attention_masks[sorted_indices]
+    labels = [labels[i] for i in sorted_indices]
+
+    # Create a mask for padding tokens
+    src_key_padding_mask = input_ids.eq(input_pad_token_id)  
+
+    batch_dict = {'input_ids': input_ids.long(),
+                  'attention_mask': attention_masks.float(),
+                  'src_key_padding_mask': src_key_padding_mask,
+                  'sorted_lengths': sorted_lengths, 
+                  'sorted_indices': sorted_indices
+                  }
+        
+    return batch_dict, torch.tensor(labels, dtype=torch.int64)
+
 
 def partial_collate_fn(b, input_pad_token_id):
     return collate_fn(b, input_pad_token_id)
@@ -148,36 +198,60 @@ class TweetDataModule(L.pytorch.LightningDataModule):
         Method to setup your datasets, here you can use whatever dataset class you have defined in Pytorch and prepare the data in order to pass it to the loaders later
         https://pytorch-lightning.readthedocs.io/en/latest/data/datamodule.html#setup
         """
-        if stage == "fit":
-            self.train_dataset = TweetDataset(self.train_df['text'], self.train_df['target'], self.tokenizer)
-            self.val_dataset = TweetDataset(self.val_df['text'], self.val_df['target'], self.tokenizer)
-            
-        # Assign test dataset for use in dataloader(s)
-        if stage == "test":
-            self.test_dataset = TweetDataset(self.test_df['text'], self.test_df['target'], self.tokenizer)
+        if self.cfg.debug_dataflow:
+            num_classes = 3
+            num_samples = num_classes * 100
+            seq_len = 32
+            self.train_dataset = RandomDataset(seq_len, num_samples, num_classes=num_classes)
+            self.val_dataset = RandomDataset(seq_len, num_samples, num_classes=num_classes)
+            self.test_dataset = RandomDataset(seq_len, num_samples, num_classes=num_classes)
+            self.sampler=None
+            self.shuffle=True
+        else:
+            if stage == "fit":
+                self.train_dataset = TweetDataset(self.train_df['text'], self.train_df['target'], self.tokenizer)
+                self.val_dataset = TweetDataset(self.val_df['text'], self.val_df['target'], self.tokenizer)
+                
+            # Assign test dataset for use in dataloader(s)
+            if stage == "test":
+                self.test_dataset = TweetDataset(self.test_df['text'], self.test_df['target'], self.tokenizer)
+
+            # Set up Random Weighted Sampling to account for class imbalance
+            counts = self.train_df['label'].value_counts()
+            class_weights = 1./counts
+            samples_weight = np.array([class_weights[t] for t in self.train_df['target']])
+
+            samples_weight = torch.from_numpy(samples_weight)
+            samples_weight = samples_weight.double()
+            self.sampler = WeightedRandomSampler(weights=samples_weight, 
+                                                num_samples=len(samples_weight), 
+                                                replacement=True)
+            if self.sampler:
+                self.shuffle=False
 
 
     def train_dataloader(self):
         return DataLoader(self.train_dataset, 
-                              batch_size=self.cfg.hyperparameters.batch_size, 
-                              shuffle=True, 
-                              collate_fn=partial(partial_collate_fn, input_pad_token_id=self.tokenizer.pad_token_id),
-                              num_workers=self.num_workers,
-                              pin_memory=self.pin_memory
-                              )
+                            batch_size=self.cfg.hyperparameters.batch_size, 
+                            sampler=self.sampler,
+                            shuffle=self.shuffle, 
+                            collate_fn=partial(partial_collate_fn, input_pad_token_id=self.tokenizer.pad_token_id),
+                            num_workers=self.num_workers,
+                            pin_memory=self.pin_memory
+                            )
 
     def val_dataloader(self):
         return DataLoader(self.val_dataset, batch_size=self.cfg.hyperparameters.batch_size, shuffle=False, 
-                          collate_fn=partial(partial_collate_fn, input_pad_token_id=self.tokenizer.pad_token_id),
-                          num_workers=self.num_workers,
-                          pin_memory=self.pin_memory
-                          )
+                        collate_fn=partial(partial_collate_fn, input_pad_token_id=self.tokenizer.pad_token_id),
+                        num_workers=self.num_workers,
+                        pin_memory=self.pin_memory
+                        )
         
 
     def test_dataloader(self):
         return DataLoader(self.test_dataset, batch_size=self.cfg.hyperparameters.batch_size, shuffle=False, 
                                     collate_fn=partial(partial_collate_fn, input_pad_token_id=self.tokenizer.pad_token_id),
-                                     num_workers=self.num_workers,
-                                     pin_memory=self.pin_memory
-                                     )
+                                    num_workers=self.num_workers,
+                                    pin_memory=self.pin_memory
+                                    )
     
